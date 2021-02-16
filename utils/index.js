08bba1dfb88faacc847320163953c451ed5c46a1
meharaj.moulasab <meharaj.moulasab@automationanywhere.com>
2021-02-16 21:03:42 +0530

    setting dev env

--- utils/index.js
+++ utils/index.js
@@ -3,8 +3,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 function isDevEnv() {
-    console.log("process.env.IS_PROD", process.env.IS_PROD)
-    return process.env.IS_PROD === 'false';
+    return process.env.IS_PROD == 'false';
 }
 
 module.exports = {
