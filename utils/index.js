76b02502fd9df45c648e7ea97de144816d829daf
meharaj.moulasab <meharaj.moulasab@automationanywhere.com>
2021-03-06 10:04:11 +0530

    added proper log for env

--- utils/index.js
+++ utils/index.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 function isDevEnv() {
-    console.log("inside isDevEnv", process.env.IS_PROD)
+    console.log("inside isDevEnv: IS_PROD", process.env.IS_PROD)
     return process.env.IS_PROD == 'false';
 }
 
